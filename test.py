83d3e20617946bdb92aadd41a57b396ec61df268
Hademinne <hademinne@gmail.com>
2023-10-23 09:21:47 +0000

    test

---
+++ test.py
@@ -0,0 +1,22 @@
+
+mylist =list()
+mylist.append('book')
+mylist.append(100)
+print(mylist)
+
+
+
+me = 'Hademinne';
+me = me.lower()
+print(me)
+
+
+me1= ['h','a','d','e','m','i','n','n','e']
+me1[2:7]
+# for letter in me1:
+#   print('letter: ', letter)
+print(me1)
+
+
+t = [1,5,6,7,9,0]
+t[1:3]
