b4f41ff77142ef083ed96ac4f8a75754499a5438
Hademinne <hademinne@gmail.com>
2023-10-23 12:55:35 +0000

    split

--- test.py
+++ test.py
@@ -1,22 +1,47 @@
 
-mylist =list()
-mylist.append('book')
-mylist.append(100)
-print(mylist)
+# mylist =list()
+# mylist.append('book')
+# mylist.append(100)
+# print(mylist)
 
 
 
-me = 'Hademinne';
-me = me.lower()
-print(me)
+# me = 'Hademinne';
+# me = me.lower()
+# print(me)
 
 
-me1= ['h','a','d','e','m','i','n','n','e']
-me1[2:7]
-# for letter in me1:
-#   print('letter: ', letter)
-print(me1)
+# me1= ['h','a','d','e','m','i','n','n','e']
+# 'x' in me1
+# print(me1[3:7])
+# # for letter in me1:
+# #   print('letter: ', letter)
 
 
-t = [1,5,6,7,9,0]
-t[1:3]
+
+# t = [1,5,6,7,9,0]
+# print(t[1:3])
+
+# mylist1 ='this,is,first,try'
+# test = mylist1.split(',')
+# print(test)
+
+# for w in mylist1:
+#   print(w)
+
+# print(test[2])
+
+
+# line ='this my email: hademinne@gmail.com'
+# adress=line.split()
+
+# email=adress[3]
+
+# pieces=email.split('@')
+
+# print(pieces[0])
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = a + b
+print(len(c))
