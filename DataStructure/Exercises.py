8db9555375b305c3e6a18d05a3079ead262d67d7
Hademinne <hademinne@gmail.com>
2023-10-30 20:48:29 +0000

    done

--- DataStructure/Exercises.py
+++ DataStructure/Exercises.py
@@ -10,21 +10,26 @@
 # print(counts)
 
 # Exercise 2: Write a program that reads a file and prints the longest line in the file. If there are multiple lines with the same length, it should print the first one.
-# file = "words.txt"
-# longest_line=''
-# max_length=0
-# fh = open(file)
-# for lines in fh:
-#     if len(lines)> max_length:
+
+file = open("words.txt")
+lg_line = ""
+max_length = 0
+
+for line in file:
+    if len(line) > max_length:
+        max_length = len(line)
+        lg_line = line
+
+print("My line: ", lg_line)
 
 
 # Exercise 3: Create a program that reads a file and prints the number of words in the file.
-hf = open("words.txt")
-counts = 0
-for line in hf:
-    word = line.split()
-    counts += len(word)
-print(counts)
+# hf = open("words.txt")
+# counts = 0
+# for line in hf:
+#     word = line.split()
+#     counts += len(word)
+# print(counts)
 
 
 # Solution1:
